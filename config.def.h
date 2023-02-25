d8745a47dfd818b3530fc1f93307595beef1461a
tfkhdyt <tfkhdyt@proton.me>
2023-02-25 15:03:15 +0700

    fix(config): reduce tab width and height

--- config.def.h
+++ config.def.h
@@ -128,8 +128,8 @@ static const unsigned int tabmodkey        = 0x40; /* (Alt) when this key is hel
 static const unsigned int tabcyclekey      = 0x17; /* (Tab) when this key is hit the menu moves one position forward in client stack. Must be the same key as used to run alttabstart */
 static const unsigned int tabposy          = 1;    /* tab position on Y axis, 0 = top, 1 = center, 2 = bottom */
 static const unsigned int tabposx          = 1;    /* tab position on X axis, 0 = left, 1 = center, 2 = right */
-static const unsigned int maxwtab          = 600;  /* tab menu width */
-static const unsigned int maxhtab          = 200;  /* tab menu height */
+static const unsigned int maxwtab          = 400;  /* tab menu width */
+static const unsigned int maxhtab          = 100;  /* tab menu height */
 #endif // ALT_TAB_PATCH
 
 /* Indicators: see patch/bar_indicators.h for options */
