2131fccdf8b50084e39e89d9c6434fb319569691
tfkhdyt <tfkhdyt@proton.me>
2023-02-25 19:48:45 +0700

    feat(rules): add floating rule for pavucontrol

--- config.def.h
+++ config.def.h
@@ -499,6 +499,7 @@ static const Rule rules[] = {
 	RULE(.wintype = WTYPE "SPLASH", .isfloating = 1)
 	RULE(.class = "Gimp", .tags = 1 << 4)
 	RULE(.class = "Firefox", .tags = 1 << 7)
+  RULE(.class = "Pavucontrol", .isfloating = 1)
 	#if RENAMED_SCRATCHPADS_PATCH
 	RULE(.instance = "spterm", .scratchkey = 's', .isfloating = 1)
 	#elif SCRATCHPADS_PATCH
