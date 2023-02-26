5f14d90a5067d8cf4f31c800c467b6ab8a9faca7
tfkhdyt <tfkhdyt@proton.me>
2023-02-26 19:52:24 +0700

    fix(scale preview): increase to 6

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const unsigned int snap           = 32;  /* snap pixel */
 static const int swallowfloating         = 0;   /* 1 means swallow floating windows by default */
 #endif // SWALLOW_PATCH
 #if BAR_TAGPREVIEW_PATCH
-static const int scalepreview            = 4;        /* Tag preview scaling */
+static const int scalepreview            = 6;        /* Tag preview scaling */
 #endif // BAR_TAGPREVIEW_PATCH
 #if NO_MOD_BUTTONS_PATCH
 static int nomodbuttons                  = 1;   /* allow client mouse button bindings that have no modifier */
