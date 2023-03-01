4124a1bcb2bf48ba5c985034e3b22a4e8dac8b1f
tfkhdyt <tfkhdyt@proton.me>
2023-03-01 18:03:12 +0700

    fix(rules): remove rule for gimp and firefox

--- config.def.h
+++ config.def.h
@@ -497,8 +497,6 @@ static const Rule rules[] = {
 	RULE(.wintype = WTYPE "UTILITY", .isfloating = 1)
 	RULE(.wintype = WTYPE "TOOLBAR", .isfloating = 1)
 	RULE(.wintype = WTYPE "SPLASH", .isfloating = 1)
-	RULE(.class = "Gimp", .tags = 1 << 4)
-	RULE(.class = "Firefox", .tags = 1 << 7)
   RULE(.class = "Pavucontrol", .isfloating = 1)
   RULE(.title = "Steam - News", .isfloating = 1)
 	#if RENAMED_SCRATCHPADS_PATCH
